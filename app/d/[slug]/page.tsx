f0d7ae840348f9fd7f9dfda70da0c92849880401
jaycokebeats-dot <jaycokebeats@gmail.com>
2026-09-24 11:13:04 -0300

    Enable dynamicParams = true for on-demand rendering

--- app/d/[slug]/page.tsx
+++ app/d/[slug]/page.tsx
@@ -8,7 +8,7 @@ const HIDDEN_SLUGS = new Set([
   "ec-detail-estetica-vehicular",
 ]);
 
-export const dynamicParams = false;
+export const dynamicParams = true;
 
 export function generateStaticParams() {
   return getAllBusinesses()
